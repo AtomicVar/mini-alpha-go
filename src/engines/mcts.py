f50603f91575052414ad974dd26ec0177844ced3
microJerry <3160103176@zju.edu.cn>
2019-05-27 11:01:18 +0800

    modified_mcts

--- src/engines/mcts.py
+++ src/engines/mcts.py
@@ -3,28 +3,30 @@ import numpy as numpy
 import random
 import math
 import datetime
+sys.path.append('..')
 from env.board import Board
 from copy import deepcopy
 class MonteCarlo(object):
-    def __init__(self, chess_board, **kwargs):
-        maxTime = kwargs.get('time',60)   
+    def __init__(self, **kwargs):
+        maxTime = kwargs.get('time',30)   
         maxMoves = kwargs.get('move',1000) #快速完成中的最大步数
         self.play_time = datetime.timedelta(seconds = maxTime)
-        self.chess_board = chess_board
+        self.chess_board = Board()
         self.cumulated_states = []
+        self.cumulated_states.append(self.chess_board)
         self.max_moves = maxMoves
         #self.current_player = B#self.chess_board.color
-        self.wins = {}  #节点的获胜次数
+        self.wins = {}  # 节点的获胜次数  索引为(player,state_matrix) 不采用(player,state)
         self.plays = {} #节点的访问次数
         self.C = kwargs.get('C', 1.414) #UCT的参数C
         self.max_depth = 0
     def update(self,state):
-        self.cumulated_state.append(state)
+        self.cumulated_states.append(state)
 
     def get_play(self):
         self.max_depth = 0
         state = self.cumulated_states[-1]
-        player = state.current_player(state)
+        player = state.color#current_player(state)
         legal = list(state.avail_steps[player].keys())
 
         # Bail out early if there is no real choice to be made.
@@ -35,41 +37,52 @@ class MonteCarlo(object):
 
         games = 0
         begin = datetime.datetime.utcnow()
-        while datetime.datetime.utcnow() - begin < self.calculation_time:
+        while datetime.datetime.utcnow() - begin < self.play_time:
             self.run_simulation()
             games += 1
 
         #moves_states = [(p, self.board.next_state(state, p)) for p in legal]
+        
+        # 不能这么来做，因为这里产生的state和之前simulation里产生的不在同一个地址的，无法索引
         moves_states = []
         for p in legal:
             state_copy = deepcopy(state)
             for avail_target in state_copy.avail_steps[player][p]:
                 state_copy.exec(p,avail_target)
-                moves_states.append((p,state_copy))
+                state_matrix = state_copy.hash_state()
+                moves_states.append((p,state_copy,state_matrix))
+                state_copy = deepcopy(state)
 
+        
         # Display the number of calls of 'run_simulation' and the 
         # time elapsed.
         print(games, datetime.datetime.utcnow() - begin)
 
         # Pick the move with the highest percentage of wins.
-        percent_wins, move = max(
-            (self.wins.get((player, S), 0) / self.plays.get((player, S), 1),
-            p) for p, S in moves_states
-        )
+        
 
+        percent_wins, move,next_state,state_mat = max(
+            ((self.wins.get((player, S_mat), 0) / self.plays.get((player, S_mat), 1), 
+            p,S,S_mat) for p, S,S_mat in moves_states),
+            key = lambda x: x[0]
+        )
+        
         # Display the stats for each possible play.
+        
         for x in sorted(
-            ((100 * self.wins.get((player, S), 0) / self.plays.get((player, S), 1),
-            self.wins.get((player, S), 0),
-            self.plays.get((player, S), 0), p)
-            for p, S in moves_states),
+            ((100 * self.wins.get((player, S_mat), 0) / self.plays.get((player, S_mat), 1),
+            self.wins.get((player, S_mat), 0),
+            self.plays.get((player, S_mat), 0), p)
+            for p, S,S_mat in moves_states),
             reverse = True
         ):
+            
             print("{3}: {0:.2f} % ({1} / {2})".format(*x))
 
         print("Maximum depth searched:", self.max_depth)
 
-        return move
+        
+        return move,next_state
     
     def run_simulation(self):  
         plays, wins = self.plays, self.wins
@@ -81,7 +94,8 @@ class MonteCarlo(object):
         expand = True
         for t in range(self.max_moves):
             state = deepcopy(states_copy[-1])
-            player = state.get_current_player()
+            player = state.color#get_current_player()
+            
             """
             legal = state.avail_steps[player].keys()#....如果不能return avail step给我，可能得做2次那个函数
 
@@ -90,46 +104,69 @@ class MonteCarlo(object):
             state.exec(choice_source,choice_target)
             """
             legal = list(state.avail_steps[player].keys())
+            #print(state.avail_steps[player])
             moves_states = []
             for p in legal:
                 state_copy = deepcopy(state)
                 for avail_target in state_copy.avail_steps[player][p]:
+                        
                     state_copy.exec(p,avail_target)
-                    moves_states.append((p,state_copy))
+                    state_matrix = state_copy.hash_state()
+                    moves_states.append((p,state_copy,state_matrix))
+                    state_copy = deepcopy(state)
             
-            if all(plays.get((player, S)) for p, S in moves_states):
+            #state_matrix 表示在当前状态下的矩阵,用字符串表示 例如：'1-11-1',用它来做索引
+            if all(plays.get((player, S_mat)) for p, S, S_mat in moves_states):
                 # UCT算法 upper confrence bound apply into tree
                 log_total = math.log(
-                    sum(plays[(player, S)] for p, S in moves_states))
-                value, move, state = max(((wins[(player, S)] / plays[(player, S)]) +
-                self.C * math.sqrt(log_total / plays[(player, S)]), p, S)
-                for p, S in moves_states)
+                    sum(plays[(player, S_mat)] for p, S, S_mat in moves_states))
+                value, move, state, state_mat = max(((wins[(player, S_mat)] / plays[(player, S_mat)]) +
+                self.C * math.sqrt(log_total / plays[(player, S_mat)]), p, S, S_mat)
+                for p, S,S_mat in moves_states)
             else:
                 # Otherwise, just make an arbitrary decision.
-                move, state = random.choice(moves_states)
+                move, state, state_mat = random.choice(moves_states)
 
-            #state = self.chess_board.next_state(state, play) #这里要改过，state自动在board里变成下个状态
+            #state = self.chess_board.next_state(state, play) #
             states_copy.append(state)
 
             # 'player' here and below refers to the player
             # who moved into that particular state
-            if expand and (player, state) not in self.plays:
+            if expand and (player, state_mat) not in self.plays:
                 expand = False
-                self.plays[(player, state)] = 0
-                self.wins[(player, state)] = 0
+                self.plays[(player, state_mat)] = 0
+                self.wins[(player, state_mat)] = 0
                 if t > self.max_depth:
                     self.max_depth = t
 
-            visited_states.add((player, state))
+            visited_states.add((player, state_mat))
 
-            player = state.get_current_player()
-            winner = state.is_terminal()
-            if winner:
+            player = state.color#get_current_player()
+            #winner = state.is_terminal
+            #if winner:
+            if state.is_terminal:
+                winner = -player
+                #print(winner)
                 break
 
-        for player, state in visited_states:
-            if (player, state) not in self.plays:
+        for player, state_mat in visited_states:
+            if (player, state_mat) not in self.plays:
                 continue
-            self.plays[(player, state)] += 1
+            #print('lalala')
+            self.plays[(player, state_mat)] += 1
             if player == winner:
-                self.wins[(player, state)] += 1
+                #print('fuckfuck')
+                self.wins[(player, state_mat)] += 1
+
+if __name__ == "__main__":
+    mc = MonteCarlo()
+    while True:
+        move,next_state = mc.get_play()
+        if move == None:
+            loser = mc.cumulated_states[-1].color
+            print('The winner is ',-loser)
+        #print(mc.wins)
+        #print(mc.plays)
+        mc.update(next_state)
+
+
